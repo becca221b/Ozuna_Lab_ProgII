a1c3bd24e6a341d35e0e222ac8f0bd9d65070e91
becca221b <chake_rebeca@hotmail.com>
2023-08-11 11:57:26 -0300

    order number inside an array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int arreglo[]= new int[3];
+
+        System.out.println("Ingrese 3 numeros");
+        Scanner scanner= new Scanner(System.in);
+        arreglo[0]= scanner.nextInt();
+        arreglo[1]= scanner.nextInt();
+        arreglo[2]= scanner.nextInt();
+
+        Arrays.sort(arreglo);
+
+        System.out.println(Arrays.toString(arreglo));
     }
+
 }
+
